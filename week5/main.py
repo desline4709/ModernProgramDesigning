fd03ecd38c589eb7cf8eee29ac659632608fbef6
30266 <302662745@qq.com>
2021-10-18 00:25:11 +0800

    recommit

--- week5/main.py
+++ week5/main.py
@@ -1,8 +1,13 @@
+from GraphStat.GraphBuilder import node as nd
 from GraphStat.GraphBuilder import graph as gh
 from GraphStat.GraphBuilder import stat
 from GraphStat.Visualization import plotgraph
 from GraphStat.Visualization import plotnodes
 
+NodeList = nd.init_node('newmovies.txt')
+# print(NodeList)
+# nd.print_node(NodeList[-1])
+# x = nd.get_attribute(NodeList[0], 'Node Name')
 
 Graph = gh.init_graph('newmovies.txt')
 # print(Graph['EdgeList'][-1])
@@ -10,6 +15,8 @@ Graph = gh.init_graph('newmovies.txt')
 # Graph1 = gh.load_graph('graph1.txt')
 # print(Graph1['NodeList'][-1])
 # x = stat.cal_average_degree(Graph)
+# print(x)
 # res = stat.cal_attr_distribute(Graph, 'Node Class')
+# print(res)
 # plotgraph.plot_degree_distribution(Graph)
-plotnodes.plot_nodes_attr(Graph, "Node Class")
+# plotnodes.plot_nodes_attr(Graph, "Node Class")
