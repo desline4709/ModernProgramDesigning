09514306b11cfd15c8811785d345057087e1f35b
30266 <302662745@qq.com>
2021-10-22 19:22:56 +0800

    completed

--- week6/main.py
+++ week6/main.py
@@ -5,26 +5,34 @@ class Tokenizer:
     def __init__(self, chars: list, coding='c', PAD=0):
         """
         Tokenizer类的初始化，构建分词词典
-        :param chars: 将要操作的文本，字符串列表
+        :param chars: 将要操作的文本，如微博数据、京东评论等，字符串列表
         :param coding: 表示分词模式，'c'表示按字分，'w'表示按词分
         :param PAD: 填充字符，默认为0
         """
-        self.status = coding
-        if self.status == 'c' or 'w':
-            keys = chars
-            keys.insert(0, 'PAD')
-            values = [i for i in range(len(keys))]
-            if PAD != 0:
-                if type(PAD) != int:
-                    # PAD是字符
-                    values[0] = PAD
-                else:
-                    # PAD是非字符且非0
-                    raise TypeError("PAD can't be in this type")
-            self.char_dict = dict(zip(keys, values))
-            self.dec_char_dict = dict(zip(values, keys))
-        else:
-            raise Exception('Error!')
+        self.__status = coding
+        self.__chars = chars
+        self.__PAD = PAD
+        temp = []
+        for i in chars:
+            if coding == 'c':
+                temp += list(i)
+            elif coding == 'w':
+                temp += jieba.lcut(i)
+            else:
+                raise Exception('Error!')
+        keys = list(set(temp))
+        keys.insert(0, 'PAD')
+        values = [i for i in range(len(keys))]
+
+        if PAD != 0:
+            if type(PAD) != int:
+                # PAD是字符
+                values[0] = PAD
+            else:
+                # PAD是非字符且非0
+                raise TypeError("PAD can't be in this type")
+        self.char_dict = dict(zip(keys, values))
+        self.dec_char_dict = dict(zip(values, keys))
 
     def tokenize(self, sentence: str):
         """
@@ -32,9 +40,9 @@ class Tokenizer:
         :param sentence: 要分词的句子
         :return: 字符串列表
         """
-        if self.status == 'c':
+        if self.__status == 'c':
             res = list(sentence)
-        elif self.status == 'w':
+        elif self.__status == 'w':
             res = jieba.lcut(sentence)
         else:
             raise Exception('Error!')
@@ -66,7 +74,7 @@ class Tokenizer:
         length = len(tokens)
         res = tokens[:]
         if length < seq_len:
-            while(length < seq_len):
+            while length < seq_len:
                 res.append(self.char_dict['PAD'])
                 length += 1
         elif length > seq_len:
@@ -82,7 +90,7 @@ class Tokenizer:
         length = len(tokens)
         res = ''
         for i in range(length):
-            if tokens[i] != 0:
+            if tokens[i] != self.__PAD:
                 try:
                     res += self.dec_char_dict[tokens[i]]
                 except KeyError:
@@ -91,24 +99,47 @@ class Tokenizer:
                 res += '[PAD]'
         return res
 
-    # def encode(self, seq_len: int):
-    #     pass
+    def encode_all(self, seq_len: int):
+        """
+        返回所有文本(chars)的长度为seq_len的tokens
+        :param seq_len: 指定长度
+        :return: tokens list
+        """
+        res = []
+        for i in self.__chars:
+            res.append(self.trim(self.encode(self.tokenize(i)), seq_len))
+        return res
+
+
+def main():
+    with open('jd_comments.txt', 'r', encoding='utf-8') as f:
+        text = f.read().split('\n')[:-1]
+
+    tok_c = Tokenizer(text, coding='c')
+    tok_w = Tokenizer(text, coding='w')
+    list_of_chars_1 = tok_c.tokenize(text[-1])
+    list_of_chars_2 = tok_w.tokenize(text[-1])
+    # print('list_of_chars1: {}'.format(list_of_chars_1))
+    # print('list_of_chars2: {}'.format(list_of_chars_2))
+    tokens1 = tok_c.encode(list_of_chars_1)
+    tokens2 = tok_w.encode(list_of_chars_2)
+    tokens1 = tok_c.trim(tokens1, 20)
+    tokens2 = tok_w.trim(tokens2, 20)
+    # print('tokens1: {}'.format(tokens1))
+    # print('tokens2: {}'.format(tokens2))
+    sentence1 = tok_c.decode(tokens1)
+    sentence2 = tok_w.decode(tokens2)
+    # print('sentence1(without trim): {}'.format(sentence1))
+    # print('sentence2(without trim): {}'.format(sentence2))
+    # print('sentence1: {}'.format(sentence1))
+    # print('sentence2: {}'.format(sentence2))
+    txt1 = tok_c.encode_all(80)
+    txt2 = tok_w.encode_all(80)
+    # print('text1_all: {}'.format(txt1))
+    # print('text2_all: {}'.format(txt2))
+
+
+
 
 if __name__ == '__main__':
-    with open('jd_comments.txt', 'r', encoding='utf-8') as f:
-        text = f.read().split('\n')
-    with open('gbChineseCharacters.txt', 'r', encoding='utf-8') as f:
-        zh_characters = ''
-        zh_characters += f.readline().strip('\n')
-        zh_characters += f.readline()
-        zh_characters = list(zh_characters)
-    # print(zh_characters[:])
-
-    tok = Tokenizer(zh_characters)
-    list_of_chars = tok.tokenize(text[0])
-    # print(list_of_chars)
-    tokens = tok.encode(list_of_chars)
-    # print(tokens)
-    tokens = tok.trim(tokens, 60)
-    sentence = tok.decode(tokens)
-    # print(sentence)
+    main()
