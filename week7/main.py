ee369b58e8991f718416e04316cb37e7f13f9d87
30266 <302662745@qq.com>
2021-10-31 20:51:54 +0800

    week7 complete

--- week7/main.py
+++ week7/main.py
@@ -1,18 +1,18 @@
 import os
 from PIL import ImageFilter, Image
 from matplotlib import pyplot as plt
-from . import mpsettings
+
 
 class Filter:
     def __init__(self, image, **kwargs):
-        self.__image = image
+        self.image = image
         self.arg_dict = kwargs
 
-    def set_image(self,img):
-        self.__image = img
+    def set_image(self, img):
+        self.image = img
 
     def get_image(self):
-        return self.__image
+        return self.image
 
     def filter(self):
         pass
@@ -23,7 +23,7 @@ class FindEdges(Filter):
         super().__init__(image, **kwargs)
 
     def filter(self):
-        self.__image = self.__image.filter(ImageFilter.FIND_EDGES)
+        self.image = self.image.filter(ImageFilter.FIND_EDGES)
 
 
 class Blur(Filter):
@@ -31,8 +31,7 @@ class Blur(Filter):
         super().__init__(image, **kwargs)
 
     def filter(self):
-        img = self.__image.filter(ImageFilter.BLUR)
-        return img
+        self.image = self.image.filter(ImageFilter.BLUR)
 
 
 class Sharpen(Filter):
@@ -40,8 +39,7 @@ class Sharpen(Filter):
         super().__init__(image, **kwargs)
 
     def filter(self):
-        img = self.__image.filter(ImageFilter.SHARPEN)
-        return img
+        self.image = self.image.filter(ImageFilter.SHARPEN)
 
 
 class Adjust(Filter):
@@ -52,20 +50,23 @@ class Adjust(Filter):
         if self.arg_dict:
             width = self.arg_dict['width']
             height = self.arg_dict['height']
-            self.__image = self.__image.resize((width, height))
+            self.image = self.image.resize((width, height))
         else:
             raise Exception('need width and height, given wrong!')
 
 
 class ImageShop:
-    def __init__(self):
+    def __init__(self, image_list=[]):
         self.flag = 0
+        self.__ins_setted = 0
         self.__mode = ''
         self.__img_path = ''  # 是一个路径
-        self.__img_instances = []
+        self.__img_instances = image_list
+        if image_list:
+            self.__ins_setted = 1
         self.__img_operated = []  # 处理过的图片
 
-    def set_path(self,path,flag):
+    def set_path(self, path, flag):
         """
         设定路径
         :param flag: 0表示是文件夹，1表示是文件
@@ -74,6 +75,9 @@ class ImageShop:
         """
         self.flag = flag
         self.__img_path = path
+
+    def get_path(self):
+        return self.__img_path
 
     def clean_operated_imgs(self):
         self.__img_operated = []
@@ -115,25 +119,60 @@ class ImageShop:
                     # 默认adjust传的参数为('adjust',(width,height))
                     f = filters[filter_names.index(i[0])](Image, width=i[1][0], height=i[1][1])
                 self.__batch_ps(f)
+                self.__img_instances = self.__img_operated[:]
+                if operations.index(i) < len(operations)-1:
+                    self.clean_operated_imgs()
 
     def display(self, rows, columns, dpi):
         plt.figure(dpi=dpi)
-        num = len(self.__img_operated)
-        for i in range(num):
-            plt.subplot(rows, columns, i+1)
-            self.__img_operated[i].show()
+        if self.__img_operated:
+            num = len(self.__img_operated)
+            for i in range(num):
+                plt.subplot(rows, columns, i + 1)
+                plt.imshow(self.__img_operated[i])
+                plt.axis('off')
+        else:
+            num = len(self.__img_instances)
+            for i in range(num):
+                plt.subplot(rows, columns, i + 1)
+                plt.imshow(self.__img_instances[i])
+                plt.axis('off')
         plt.show()
 
     def save(self, rows, columns, path, filename, dpi):
         plt.figure(dpi=dpi)
-        num = len(self.__img_operated)
-        for i in range(num):
-            plt.subplot(rows, columns, i+1)
-            self.__img_operated[i].show()
-        plt.savefig(path+'\\'+filename)
+        if self.__img_operated:
+            num = len(self.__img_operated)
+            for i in range(num):
+                plt.subplot(rows, columns, i + 1)
+                plt.imshow(self.__img_operated[i])
+                plt.axis('off')
+        else:
+            num = len(self.__img_instances)
+            for i in range(num):
+                plt.subplot(rows, columns, i + 1)
+                plt.imshow(self.__img_instances[i])
+                plt.axis('off')
+        if path:
+            plt.savefig(path + '\\' + filename)
+        else:
+            plt.savefig(filename)
+        print('image has been saves in {}'.format(os.getcwd()))
 
     def get_op_nums(self):
         return len(self.__img_operated)
+
+    def get_ins_nums(self):
+        return len(self.__img_instances)
+
+    def get_operated_imgs(self):
+        return self.__img_operated
+
+    def if_loaded(self):
+        if self.__ins_setted:
+            return True
+        else:
+            return False
 
 
 class TestImageShop:
@@ -155,48 +194,81 @@ class TestImageShop:
         :return:
         """
         filter_names = ['blur', 'find_edges', 'sharpen', 'adjust']
-        self.shop.set_path(filepath, flag)
-        self.shop.set_mode(mode)
-        self.shop.load_images()
+        if not self.shop.if_loaded():
+            self.shop.set_path(filepath, flag)
+            self.shop.set_mode(mode)
+            self.shop.load_images()
         self.shop.clean_operated_imgs()
         op_list = []
         for i in range(4):
             if filter[i] == 1:
                 if i != 3:
-                    temp = (filter[i], -1)
+                    temp = (filter_names[i], -1)
                 else:
                     if kwargs:
                         width = kwargs['width']
                         height = kwargs['height']
-                        temp = (filter[i], (width, height))
+                        temp = (filter_names[i], (width, height))
                     else:
                         raise Exception
                 op_list.append(temp)
         self.shop.batch_ps(op_list)
         op_num = self.shop.get_op_nums()
+        if not op_num:
+            op_num = self.shop.get_ins_nums()
         if plot[1] == 0:
             rows = plot[0]
-            columns = op_num // rows + 1
+            if (op_num % rows) != 0:
+                columns = op_num // rows + 1
+            else:
+                columns = op_num // rows
         else:
             columns = plot[0]
-            rows = op_num // columns + 1
+            if (op_num % columns):
+                rows = op_num // columns + 1
+            else:
+                rows = op_num // columns
         self.shop.display(rows, columns, dpi)
         self.shop.save(rows, columns, savepath, savename, dpi)
 
+    def get_operated_imgs(self):
+        return self.shop.get_operated_imgs()
+
+
+
+def test_filter():
+    im = Image.open('a.jpg')
+    # im.show()
+    # bl = Blur(im)
+    # bl.filter()
+    # im1 = bl.get_image()
+    # im1.save('blur.png')
+    fe = FindEdges(im)
+    fe.filter()
+    im2 = fe.get_image()
+    # im2.save('findedges.png')
+    # sp = Sharpen(im2)
+    # sp.filter()
+    # im3 = sp.get_image()
+    # im3.save('sharpen.png')
+    ad1 = Adjust(im, width=20, height=20)
+    ad1.filter()
+    im4 = ad1.get_image()
+    im4.save('ad1.png')
+    ad2 = Adjust(im, width=500, height=500)
+    ad2.filter()
+    im5 = ad2.get_image()
+    im5.save('ad2.png')
 
 
 if __name__ == '__main__':
-    im = Image.open('a.jpg')
-    # im.show()
-    bl = Blur(im)
-    im1 = bl.filter()
-    # im1.show()
-    fe = FindEdges(im)
-    im2 = fe.filter()
-    im2.show()
-    sp = Sharpen(im2)
-    im3 = sp.filter()
-    # im3.show()
-    ad = Adjust(im)
-    im4 = ad.filter(500, 500)
-    # sim4.show()
+    IS = ImageShop()
+    tis = TestImageShop(IS)
+    tis.operate_image(mode='jpg', filepath='imgs', savepath='', savename='FAimgs.png', flag=0, filter=(0, 1, 0, 1),
+                      plot=(3, 1), dpi=300, width=1280, height=800)
+    fa_imgs = tis.get_operated_imgs()
+    IS1 = ImageShop(fa_imgs)
+    tis1 = TestImageShop(IS1)
+    tis1.operate_image(mode='jpg', filepath='imgs', savepath='', savename='FASimgs.png', flag=0, filter=(0, 0, 1, 1),
+                      plot=(3, 1), dpi=300, width=1280, height=800)
+
